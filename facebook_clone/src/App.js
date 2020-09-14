5bbd0762ee12eaf22948f4fb0e4e326ac5896b4f
bsihame <sihamebazi@pursuit.org>
2020-09-14 09:19:19 -0400

    add Header component js and css files. add logo Facebook in the left header

--- facebook_clone/src/App.js
+++ facebook_clone/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-
+import Header from "./Header"
 import "./App.css";
 
 function App() {
 	return (
-		<div className="app">
-			<h1>Let's build a Facebook clone</h1>
+		<div className="App">
+      <h1>Let's build a Facebook clone</h1>
+
+      <Header />
+
+      {/* App Body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+
 		</div>
 	);
 }
